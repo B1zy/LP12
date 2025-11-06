622a57a7fdbe372501b2ce4d70a0fc71dda7b2eb
Kamil Bielski <K.Bielski.inf22@stud.bbbaden.ch>
2025-11-06 15:08:29 +0100

    first tutorial finished

--- util.py
+++ util.py
@@ -3,7 +3,7 @@ import cv2
 
 def get_limits(color):
 
-    c = np.vint8([color])
+    c = np.uint8([[color]])
     hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
 
     lowerLimit = hsvC[0][0][0] - 10, 100, 100
